e1d04a6af4a94f8f84f4a4526b6d512dbceeff87
daiiz <da.iz.mg@gmail.com>
2015-09-25 01:29:52 +0900

    画像を出力できるようになった

--- foo.py
+++ foo.py
@@ -7,9 +7,24 @@ from PIL import Image
 
 DEFAULT_WIDTH = 400;
 
-def generateImage():
-    #
-    pass;
+# color辞書（とりあえず決め打ち）
+COLORS = {
+    "?": (255, 255, 255),   # 不明な文字
+    "*": (0, 0, 0)          # 終端文字
+};
+
+def generateImage(colors):
+    height = len(colors) / DEFAULT_WIDTH + 1;
+    width  = DEFAULT_WIDTH;
+    canvas = Image.new("RGB", (width, height), (0,0,0));
+
+    for y in range(0, height):
+        for x in range(0, width):
+            if width * y + x < len(colors):
+                canvas.putpixel((x,y), colors[width * y + x]);
+
+    canvas.save("px/test-0.png");
+
 
 # テキストファイルを一文字ずつ読み込む
 def readChar(filePath):
@@ -20,16 +35,31 @@ def readChar(filePath):
     if os.path.isfile(filePath) is not True:
         return;
 
-    # 一文字ずつ読み取り、カラーを決定して保持する
+    # 一行ずつ読み取り、カラーを決定して保持する
+    f = open(filePath);
+    line = f.readline();
+    while line:
+        if len(line) > 0:
+            # 一文字ずつ読む
+            for c in line:
+                if c != '\n':
+                    # colorをゲット
+                    if COLORS.has_key(c) is True:
+                        cColor = COLORS[c];
+                    else:
+                        cColor = COLORS['?'];
+                    colors.append(cColor);
 
+        # 次の行を見る
+        line = f.readline();
+    f.close;
+    colors.append(COLORS['*']); #終端
 
     # 総文字数から画像サイズを決定する
     charNums = len(colors);
     if charNums > 0:
         generateImage(colors);
 
-    print charNums;
-    pass;
 
 # 読み込んだ文字からピクセルデータを決定する
 def determinePxColor(char):
