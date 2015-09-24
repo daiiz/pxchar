764a4396300fa5c50c7c129bc24ce1cfdd597c03
daiiz <da.iz.mg@gmail.com>
2015-09-24 17:03:40 +0900

    add foo.py

---
+++ foo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Project pxchar
+
+import sys
+import os.path
+from PIL import Image
+
+# 初期化
+def init():
+    pass;
+
+# テキストファイルを一文字ずつ読み込む
+def readChar(fileName):
+    pass;
+
+# 読み込んだ文字からピクセルデータを決定する
+def determinePxColor(char):
+    pass;
+
+# PNGファイルを出力する
+def applyPxColor(color):
+    pass;
+
+if __name__ == '__main__':
