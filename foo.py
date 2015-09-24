df92ea22028d5789f16a9e403b76ccc81e216e87
daiiz <da.iz.mg@gmail.com>
2015-09-24 17:26:25 +0900

    画像生成できそう

--- foo.py
+++ foo.py
@@ -5,22 +5,36 @@ import sys
 import os.path
 from PIL import Image
 
-# 初期化
-def initImage(filePath):
+DEFAULT_WIDTH = 400;
 
+def generateImage():
+    #
     pass;
 
 # テキストファイルを一文字ずつ読み込む
 def readChar(filePath):
+    # カラーデータを保持する配列
+    colors = [];
+
+    # 有効なパスかどうか判定
+    if os.path.isfile(filePath) is not True:
+        return;
+
+    # 一文字ずつ読み取り、カラーを決定して保持する
+
+
+    # 総文字数から画像サイズを決定する
+    charNums = len(colors);
+    if charNums > 0:
+        generateImage(colors);
+
+    print charNums;
     pass;
 
 # 読み込んだ文字からピクセルデータを決定する
 def determinePxColor(char):
     pass;
 
-# PNGファイルを出力する
-def applyPxColor(color):
-    pass;
 
 if __name__ == '__main__':
     print 'Hello!';
